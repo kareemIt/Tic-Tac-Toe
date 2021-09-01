54238d081f31b41c25b160f4d9d78198cce9acdd
kareemIt <kareem.itani2@gmail.com>
2021-09-01 14:41:39 -0400

    Setting up Tic-Tac-Toe

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
+import Square from '/Squares.js';
 
 export default function App() {
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <Square value={1} />
+      <Square value={2} />
+      <Square value={3} />
+      <Square value={4} />
+      <Square value={5} />
+      <Square value={6} />
+      <Square value={7} />
+      <Square value={8} />
+      <Square value={9} />
     </div>
   );
 }
