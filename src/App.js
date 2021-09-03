72696fc96d9a3023ff6f7031391017b4b3447195
kareemIt <kareem.itani2@gmail.com>
2021-09-03 01:02:48 -0400

    updating the board

--- src/App.js
+++ src/App.js
@@ -1,20 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css';
 import Square from './component/Square.js';
 
 export default function App() {
-  const lines = [
-		[0, 1, 2],
-		[3, 4, 5],
-		[6, 7, 8],
-		[0, 3, 6],
-		[1, 4, 7],
-		[2, 5, 8],
-		[0, 4, 8],
-		[2, 4, 6],
-	];
-  fucntion winGame() {
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [xIsNext, SetXIsNext] = useState(true);
+  const winner = winGame(board);
 
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+
+  function winGame(Square) {
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        square[a] &&
+        Square[a] == square[b] &&
+        Square[b] == square[c] &&
+        Square[c]
+      ) {
+        return Square[a];
+      }
+    }
+    return;
   }
 
   return (
